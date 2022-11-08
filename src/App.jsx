c459243fccbb2b832e28172dee1d6ef1583d2712
Gui Parpineli <guiparpineli@GuiPC>
2022-11-07 22:47:45 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,83 @@
-
 // Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
 
+import {useState} from "react";
+import {Card} from "./Card";
+
+const validateColorName = (colorName) => {
+    const withoutSpaces = colorName.trim();
+    return withoutSpaces.length > 2 ? withoutSpaces : false
+}
+
+const validateColorHex = (colorHex) => {
+    const withoutSpaces = colorHex.trim();
+    const split = withoutSpaces.split("")
+    console.log(split)
+    console.log(split[0])
+    return withoutSpaces.length > 5 && split[0] === "#" ? withoutSpaces : false;
+}
+
+
 function App() {
-  // Aqui você irá criar os Estados para manipular os Inputs
+    // Aqui você irá criar os Estados para manipular os Inputs
 
+    const [allCards, setAllCards] = useState([])
+    const [colorName, setColorName] = useState('')
+    const [colorHex, setColorHex] = useState('')
+    const [formularyError, setFormularyError] = useState(false)
 
-  return (
-    <div className="App">
-     <h1>Carga de estudiantes</h1>
-     <form>
-      {/* Comece a desenvolver o seu Código por aqui :) */}
-     </form>
-    </div>
-  )
+    function createCard(event) {
+        event.preventDefault()
+
+        const newCard = {
+            color: colorName,
+            codigo: colorHex,
+        }
+        console.log(validateColorHex(colorName))
+        if (!validateColorName(colorName) || validateColorHex(colorHex)) {
+            setFormularyError(true)
+        }else{
+            setFormularyError(false)
+
+            setAllCards([...allCards, newCard])
+
+            setColorHex('')
+            setColorName('')
+        }
+
+    }
+
+    return (
+        <div className="App">
+            <h1>Carga de estudiantes</h1>
+            <form onSubmit={createCard} className={formularyError? 'form-error' : ''}>
+                <label htmlFor="colorName">Nome</label>
+                <input type="text" value={colorName} onChange={event => setColorName(event.target.value)}/>
+
+                <label htmlFor="colorHex">Codigo da cor</label>
+                <input type="text" value={colorHex} onChange={event => setColorHex(event.target.value)}/>
+
+                <button type='submit'>Adicionar</button>
+
+            </form>
+            {
+                formularyError ? (
+                    <span>O seu formulário contem erros</span>
+                ) : null
+            }
+            <div>
+                {
+                    allCards.map(
+                        cards =>(
+                            <Card
+                                values={cards}
+                            />
+                        )
+
+                    )
+                }
+            </div>
+        </div>
+    )
 }
 
 export default App
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,10 @@
 //Este componente deverá receber dados por Props e mostrar as Informações em Tela
 
-export function Card () {
-  return (
-    <div>
-
-    </div>
-  )
+export function Card(props) {
+    return (
+        <div>
+            <p>{props.values.color}</p>
+            <h1>{props.values.codigo}</h1>
+        </div>
+    )
 }
